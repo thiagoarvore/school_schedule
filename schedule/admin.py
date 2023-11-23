ad9dfd244ecb4295b9fe89be56ec7adf3ddca039
thiagoarvore <137798387+thiagoarvore@users.noreply.github.com>
2023-11-23 16:14:51 -0300

    db_setup

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from schedule.models import Turma, Disciplina, WeekDay, Professor
+
+class TurmasAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class WeekDayAdmin(admin.ModelAdmin):
+    list_display = ('name', 'aulas_diarias',)
+    search_fields = ('name',)    
+
+class DisciplinaAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantidade_de_aulas_semana', 'aula_dividida',)
+    search_fields = ('name',)
+
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    search_fields = ('name',)    
+
+
+admin.site.register(Turma, TurmasAdmin)
+admin.site.register(WeekDay, WeekDayAdmin)
+admin.site.register(Disciplina, DisciplinaAdmin)
+admin.site.register(Professor, ProfessorAdmin)
--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Disciplina(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    quantidade_de_aulas_semana = models.IntegerField()
+    aula_dividida = models.BooleanField()
+
+    def __str__(self):
+        return self.name
+
+class Turma(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=10) #campo de string
+    disciplinas = models.ManyToManyField(Disciplina, related_name='disciplinas')
+
+    def __str__(self):
+        return self.name
+
+class WeekDay(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    aulas_diarias = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Professor(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    disciplina = models.ManyToManyField(Disciplina, related_name='disciplina')
+    disponibilidade = models.ManyToManyField(WeekDay, related_name='disponibilidade')
+    turmas = models.ManyToManyField(Turma, related_name='turmas')
+
+    def __str__(self):
+        return self.name
